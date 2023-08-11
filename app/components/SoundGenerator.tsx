c831d242a0d45cfa79a02f1187d14145f85a6f13
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-08-11 16:45:26 +0530

    Created a nice looking UI for my Sound Generator pp

--- app/components/SoundGenerator.tsx
+++ app/components/SoundGenerator.tsx
@@ -1,29 +1,39 @@
 "use client";
 import { useState, useRef } from "react";
-import { GrPlayFill } from "react-icons/gr";
-import { MdOutlineReplay } from "react-icons/md";
+// import { BsFillPlayCircleFill } from "react-icons/bs";
+import { MdReplay, MdPlayArrow } from "react-icons/md";
 
 const SoundGenerator = ({ soundFiles }: { soundFiles: string[] }) => {
   const [randomSound, setRandomSound] = useState(
     soundFiles[Math.floor(Math.random() * soundFiles.length)]
   );
+  const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
+  function togglePlay() {
+    console.log(audioRef);
+    if (!isPlaying) {
+      audioRef?.current?.play().then(() => {
+        setIsPlaying(true);
+      });
+      // setIsPlaying(true);
+    } else {
+      audioRef?.current?.pause();
+      setRandomSound(soundFiles[Math.floor(Math.random() * soundFiles.length)]);
+      setIsPlaying(false);
+    }
+  }
 
   return (
-    <div>
-      <audio
-        ref={audioRef}
-        controls
-        src={randomSound}
-        onPause={() =>
-          setRandomSound(
-            soundFiles[Math.floor(Math.random() * soundFiles.length)]
-          )
-        }
-      ></audio>
-      {/* <button onClick={togglePlay}>
-        {isPlaying ? <MdOutlineReplay /> : <GrPlayFill />}
-      </button> */}
+    <div className="ml-8">
+      <audio ref={audioRef} src={randomSound}></audio>
+      <button
+        onClick={togglePlay}
+        className={`hover:scale-105 addshadow flex bg-white rounded-full w-[70px] h-[70px] justify-center items-center  ${
+          isPlaying ? "text-3xl" : `text-6xl`
+        }`}
+      >
+        {isPlaying ? <MdReplay /> : <MdPlayArrow />}
+      </button>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
 import GetSound from "./components/GetSound";
-
+import Frankenstein from "../public/Frankenstein.png";
+import Image from "next/image";
 function Home() {
   return (
-    <div>
-      <GetSound />
+    <div className="mx-8">
+      <Card className="w-[380px] bg-[#7E22CE]/60 p-4">
+        <CardContent className="flex justify-between">
+          <Image src={Frankenstein} alt="Mind Blown" height={190} width={100} />
+          <div className="flex flex-col justify-center gap-6 p-4">
+            <CardTitle className="text-3xl">Just Play</CardTitle>
+
+            <GetSound />
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
